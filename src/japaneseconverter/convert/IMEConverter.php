4e8d3409ec4e8b998a5434861f51f06432c94871
shun <80146606+shunnyan@users.noreply.github.com>
2023-09-08 03:31:21 +0900

    クソコを修正

--- src/japaneseconverter/convert/IMEConverter.php
+++ src/japaneseconverter/convert/IMEConverter.php
@@ -6,12 +6,10 @@ class IMEConverter
 {
 	public static $GOOGLE_IME_URL = "https://www.google.com/transliterate?langpair=ja-Hira|ja&text=";
 	public static function GIME_Convert(String $text): String{
-		$text = str_replace("　","%E3%80%80",$text);
-		$text = str_replace(" ","%20",$text);
-		$convert = file_get_contents(self::$GOOGLE_IME_URL . $text);
+		$convert = file_get_contents(self::$GOOGLE_IME_URL . urlencode($text));
 		foreach (json_decode($convert,true) as $key => $value){
 			$text = str_replace($value[0],$value[1][0],$text);
 		}
-		return str_replace("%20"," ",$text);
+		return $text;
 	}
 }
