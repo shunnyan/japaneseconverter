795116816b391a20c73d875747148c43c0107582
shun <80146606+shunnyan@users.noreply.github.com>
2023-09-08 03:48:25 +0900

    コメントアウト消去

--- src/japaneseconverter/convert/Converter.php
+++ src/japaneseconverter/convert/Converter.php
@@ -6,7 +6,6 @@ class Converter
 {
 	public static function isNeedJapanize(String $text): bool{
 		if (mb_strlen($text, 'UTF-8') == strlen($text)) {
-			// 正規表現を使用して全角カタカナ文字を検出
 			if (preg_match('/[Ａ-Ｚａ-ｚ０-９ａ-ｚ]/u', $text)) {
 				return false;
 			}
