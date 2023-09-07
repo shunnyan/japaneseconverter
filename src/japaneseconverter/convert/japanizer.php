21799e65d7101ffe74818e455fc746cdb8cc2d11
shun <80146606+shunnyan@users.noreply.github.com>
2023-09-08 03:49:19 +0900

    余分な空白を除去

--- src/japaneseconverter/convert/japanizer.php
+++ src/japaneseconverter/convert/japanizer.php
@@ -14,10 +14,10 @@ class japanizer
 		$text = KanaConverter::convert($text);
 		$replace = MessageConfig::$MainConfig->get("all_replace") ?? false;
 		if(MessageConfig::$MainConfig->get("kana_only") ?? false){
-			return $replace ? $text : $base . " §r§6( " . $text . " §r§6)";
+			return $replace ? $text : $base . " §r§6(" . $text . "§r§6)";
 		}else{
 			$con = IMEConverter::GIME_Convert($text);
-			return $replace ? $con : $base . " §r§6( " . $con . " §r§6)";
+			return $replace ? $con : $base . " §r§6(" . $con . "§r§6)";
 		}
 	}
 
